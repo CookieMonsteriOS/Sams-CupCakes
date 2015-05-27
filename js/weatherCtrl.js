5691e627baf5d3db6ceb20c9456e6cfd328e87de
sam chaudry <sam.chaudry@hotmail.co.uk>
2015-05-27 21:55:36 +0100

    Module seperations

--- js/weatherCtrl.js
+++ js/weatherCtrl.js
@@ -1,4 +1,3 @@
-var app = angular.module('userweather', []);
 
 app.controller('weatherController', function($scope, $http) {
     $http.get("http://api.openweathermap.org/data/2.5/weather?q=Leeds,uk")
